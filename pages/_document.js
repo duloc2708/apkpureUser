2ca5950761fdac74e7be749eb465d9cf4c3f9100
Loc Du <locdu@kolabs.co>
2019-02-24 15:07:11 +0700

    update css

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,5 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
-import stylesheet2 from 'styles/font-awesome.min.css';
-import stylesheet3 from 'styles/animate.min.css';
-import stylesheet1 from 'styles/style.css';
-
-
 export default class SiteDocument extends Document {
   componentDidMount() {
 
@@ -17,9 +12,9 @@ export default class SiteDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="robots" content="INDEX, FOLLOW" />
           <meta name="Googlebot" content="index,follow,NOODP" />
-          <style global dangerouslySetInnerHTML={{ __html: stylesheet2 }} />
-          <style global dangerouslySetInnerHTML={{ __html: stylesheet3 }} />
-          <style global dangerouslySetInnerHTML={{ __html: stylesheet1 }} />
+          <link rel="stylesheet" href="http://apksafety.com/static/styles/font-awesome.min.css"></link>
+          <link rel="stylesheet" href="http://apksafety.com/static/styles/animate.min.css"></link>
+          <link rel="stylesheet" href="http://apksafety.com/static/styles/style.css"></link>
 
         </Head>
         <body className="blog-class">
@@ -27,20 +22,20 @@ export default class SiteDocument extends Document {
             <Main />
           </div>
           <NextScript />
-          <script src="/static/js/vendor/jquery-2.2.4.min.js"></script>
-          <script src="/static/js/popper.min.js"></script>
-          <script src="/static/js/vendor/bootstrap.min.js"></script>
+          <script src="http://apksafety.com/static/js/vendor/jquery-2.2.4.min.js"></script>
+          <script src="http://apksafety.com/static/js/popper.min.js"></script>
+          <script src="http://apksafety.com/static/js/vendor/bootstrap.min.js"></script>
           <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-          <script src="/static/js/easing.min.js"></script>
-          <script src="/static/js/superfish.min.js"></script>
-          <script src="/static/js/jquery.ajaxchimp.min.js"></script>
-          <script src="/static/js/jquery.magnific-popup.min.js"></script>
-          <script src="/static/js/owl.carousel.min.js"></script>
-          <script src="/static/js/jquery.tabs.min.js"></script>
-          <script src="/static/js/mn-accordion.js"></script>
-          <script src="/static/js/jquery.nice-select.min.js"></script>
-          <script src="/static/js/mail-script.js"></script>
-          <script src="/static/js/main.js"></script>
+          <script src="http://apksafety.com/static/js/easing.min.js"></script>
+          <script src="http://apksafety.com/static/js/superfish.min.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.ajaxchimp.min.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.magnific-popup.min.js"></script>
+          <script src="http://apksafety.com/static/js/owl.carousel.min.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.tabs.min.js"></script>
+          <script src="http://apksafety.com/static/js/mn-accordion.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.nice-select.min.js"></script>
+          <script src="http://apksafety.com/static/js/mail-script.js"></script>
+          <script src="http://apksafety.com/static/js/main.js"></script>
         </body>
       </html>
     )
