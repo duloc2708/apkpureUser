6b7e1948eb3d43e0cdb5582096323f9af496004a
Loc Du <locdu@kolabs.co>
2019-03-05 21:21:29 +0700

    update code

--- src/modules/down/DowloadFormView.js
+++ src/modules/down/DowloadFormView.js
@@ -1,98 +1,51 @@
 
 import Link from 'next/link'
+import HotUpdateFormView from 'modules/down/HotUpdateFormView'
+import LastReleaseFormView from 'modules/down/LastReleaseFormView'
 
 class DowloadFormView extends React.Component {
     constructor(props) {
         super(props);
     }
     componentDidMount() {
-        setTimeout(() => {
-            const { name, mineType } = this.props.data
-            const link = document.createElement('a');
-            link.href = `${Config.API_DOWNLOAD_FILE}articles/getfileapk?namefile=${name}&mineType=${mineType}`
-            link.setAttribute('download', 'download'); //or any other extension
-            document.body.appendChild(link);
-            link.click();
-        }, 3000)
+        // setTimeout(() => {
+        //     const { name, mineType } = this.props.data
+        //     const link = document.createElement('a');
+        //     link.href = `${Config.API_DOWNLOAD_FILE}articles/getfileapk?namefile=${name}&mineType=${mineType}`
+        //     link.setAttribute('download', 'download'); //or any other extension
+        //     document.body.appendChild(link);
+        //     link.click();
+        // }, 3000)
     }
     render() {
         let { title, type_name, type, thumbnail, content_long, title_slug, atr3 } = this.props.data
         return (
-            <section className="popular-post-area pt-120">
-                <div className="container">
-                    <div className="row">
-                        <div className="title d-flex flex-column col-lg-12">
-                            <h4>Download</h4>
-                            <span></span>
+            <React.Fragment>
+                <section className="popular-post-area download__time pt-120">
+                    <div className="container bg__time">
+                        <h3>Downloading Crafting Idle Clicker (Mod Money) 4.1.7mod</h3>
+                        <p>Your link are ready</p>
+                        <div className="download__time__btn">
+                            <button type="button" className="btn btn-success">Download APK</button>
+                            {/* <button type="button" className="btn btn-success">Download Data Main</button>
+                            <button type="button" className="btn btn-success">Download Data Patch</button> */}
                         </div>
-                        <section className="page__download">
-                            <div className="title__download section-title">
-                                <span><a href="#">HOT UPDATE</a></span>
-                            </div>
-                            <div className="section-content">
-                                <div className="items">
-                                    <a className="favicon" href="#" title="Pixel Z Gunner 3D">
-                                        <img src={`${Config.API_LOCAL_IMAGE}img/download/rogue-agents_icon.png`} />
-                                    </a>
-                                    <div className="name">
-                                        <a href="#" title="Pixel Z Gunner 3D">Pixel Z Gunner 3D - Battle Survival Fps (Free Shopping) 4.3</a>
-                                    </div>
-                                    <div className=" author">
-                                        <a href="#" className="_search_author"> PixelStar Games </a>
-                                    </div>
-                                    <div className="ver">
-                                        <a>4.3 </a>
-                                    </div>
-                                    <div className="btn-download">
-                                        <a href="#">
-                                            <i className="fa fa-download" aria-hidden="true"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        <section className="latest box-white block">
-                            <div className="title__download section-title">
-                                <span><a href="#">LATEST RELEASES</a></span>
-                            </div>
-                            <div className="section-content">
-                                <ul>
-                                    <li className="item">
-                                        <div className="row">
-                                            <div className="col-md-7">
-                                                <a href="#" className="img-item">
-                                                    <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
-                                                </a>
-                                                <div className="info">
-                                                    <a href="#" title="Folio for Facebook 3.2.13">
-                                                        Folio for Facebook 3.2.13
-                                                </a>
-                                                    <div className="new-chap author">
-                                                        <a href="#" className="search_author"> Creative Labs LLC </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-3">
-                                                <div className="vol-chap ver text-left">
-                                                    <p>3.2.13</p>
-                                                    <p className="new-chap">27.02.2019</p>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-2">
-                                                <div className="btn-download">
-                                                    <a href="#">
-                                                        <i className="fa fa-download" aria-hidden="true"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </section>
                     </div>
-                </div>
-            </section>
+                </section>
+                <section className="popular-post-area">
+                    <div className="container">
+                        <div className="row">
+                            <div className="title d-flex flex-column col-lg-12">
+                                <h4>Download</h4>
+                                <span></span>
+                            </div>
+                            <HotUpdateFormView />
+                            <LastReleaseFormView/>
+                        </div>
+                    </div>
+                </section>
+
+            </React.Fragment>
         )
     }
 }
---
+++ src/modules/down/LastReleaseFormView.js
@@ -0,0 +1,303 @@
+
+import Link from 'next/link'
+
+class LastReleaseFormView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        // setTimeout(() => {
+        //     const { name, mineType } = this.props.data
+        //     const link = document.createElement('a');
+        //     link.href = `${Config.API_DOWNLOAD_FILE}articles/getfileapk?namefile=${name}&mineType=${mineType}`
+        //     link.setAttribute('download', 'download'); //or any other extension
+        //     document.body.appendChild(link);
+        //     link.click();
+        // }, 3000)
+    }
+    render() {
+        return (
+            <section className="latest box-white block">
+                <div className="title__download section-title">
+                    <span><a href="#">LATEST RELEASES</a></span>
+                </div>
+                <div className="section-content">
+                    <ul>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="item">
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <a href="#" className="img-item">
+                                        <img className="lazy loaded" alt="Folio for Facebook 3.2.13" title="Folio for Facebook 3.2.13" src="https://static.appvn.com/a/uploads/thumbnails/022016/folio-for-facebook_icon.png" />
+                                    </a>
+                                    <div className="info">
+                                        <a href="#" title="Folio for Facebook 3.2.13">
+                                            Folio for Facebook 3.2.13
+                    </a>
+                                        <div className="new-chap author">
+                                            <a href="#" className="search_author"> Creative Labs LLC </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="vol-chap ver text-left">
+                                        <p>3.2.13</p>
+                                        <p className="new-chap">27.02.2019</p>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="btn-download">
+                                        <a href="#">
+                                            <i className="fa fa-download" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+        )
+    }
+}
+export default LastReleaseFormView
