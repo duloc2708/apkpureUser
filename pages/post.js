53f6afc28b751cf03e56c71e4f003c722cfa6fc0
Loc Du <locdu@kolabs.co>
2019-03-06 18:03:32 +0700

    update code

--- pages/post.js
+++ pages/post.js
@@ -63,23 +63,25 @@ const Post = (props) => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       {/* <title>Đài truyền hình Hàn Quốc làm tin hội nghị thượng đỉnh như thế nào? | Tinhte.vn</title> */}
       <title>{props.data.title}</title>
-      <link rel="apple-touch-icon" href="https://tinhte.vn/styles/uiflex/dimota/logo.og.png" />
-      <link rel="alternate" type="application/rss+xml" title="RSS của Tinhte.vn" href="https://tinhte.vn/rss/" />
-      <link rel="next" href="threads/dai-truyen-hinh-han-quoc-lam-tin-hoi-nghi-thuong-dinh-nhu-the-nao.2920754/page-2" />
-      <link rel="canonical" href="https://tinhte.vn/threads/dai-truyen-hinh-han-quoc-lam-tin-hoi-nghi-thuong-dinh-nhu-the-nao.2920754/" />
-      <meta name="description" content="Trong thời gian này, khi mà cuộc họp thượng đỉnh Hoa Kỳ - Triều Tiên đang chuẩn bị diễn ra tại Hà Nội thì việc đưa tin nhanh nhất, hiệu quả nhất được..." /><meta property="author" content="kevin_pump" /><meta property="article:author" content="https://facebook.com/10208889536421702" />
-      <meta property="og:site_name" content="Tinhte.vn" />
+      <link rel="apple-touch-icon" href='http://apksafety.com/static/img/logo-d.png' />
+      {/* <link rel="alternate" type="application/rss+xml" title="RSS của Tinhte.vn" href="https://tinhte.vn/rss/" /> */}
+      {/* <link rel="next" href="threads/dai-truyen-hinh-han-quoc-lam-tin-hoi-nghi-thuong-dinh-nhu-the-nao.2920754/page-2" /> */}
+      <link rel="canonical" href={props.data.title} />
+      <meta name="description" content={props.data.title} />
+      {/* <meta property="author" content="kevin_pump" /> */}
+      {/* <meta property="article:author" content="https://facebook.com/10208889536421702" /> */}
+      <meta property="og:site_name" content="Apksafety.com" />
       <meta property="og:type" content="article" />
-      <meta property="og:url" content="https://tinhte.vn/threads/dai-truyen-hinh-han-quoc-lam-tin-hoi-nghi-thuong-dinh-nhu-the-nao.2920754/" />
-      <meta property="og:title" content="Đài truyền hình Hàn Quốc làm tin hội nghị thượng đỉnh như thế nào?" />
-      <meta property="og:description" content="Trong thời gian này, khi mà cuộc họp thượng đỉnh Hoa Kỳ - Triều Tiên đang chuẩn bị diễn ra tại Hà Nội thì việc đưa tin nhanh nhất, hiệu quả nhất được..." />
-      <meta property="fb:app_id" content="145923738846814" />
+      <meta property="og:url" content={props.data.title} />
+      <meta property="og:title" content={props.data.title} />
+      <meta property="og:description" content={props.data.title} />
+      {/* <meta property="fb:app_id" content="145923738846814" /> */}
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:title" content="Đài truyền hình Hàn Quốc làm tin hội nghị thượng đỉnh như thế nào?" />
-      <meta property="twitter:description" content="Trong thời gian này, khi mà cuộc họp thượng đỉnh Hoa Kỳ - Triều Tiên đang chuẩn bị diễn ra tại Hà Nội thì việc đưa tin nhanh nhất, hiệu quả nhất được..." />
-      <meta property="twitter:url" content="https://tinhte.vn/threads/dai-truyen-hinh-han-quoc-lam-tin-hoi-nghi-thuong-dinh-nhu-the-nao.2920754/" />
-      <meta property="og:image" content="https://photo2.tinhte.vn/data/attachment-files/2019/02/4576910_Cover.jpg" />
-      <meta property="twitter:image" content="https://photo2.tinhte.vn/data/attachment-files/2019/02/4576910_Cover.jpg" />
+      <meta property="twitter:title" content={props.data.title} />
+      <meta property="twitter:description" content={props.data.title} />
+      <meta property="twitter:url" content={props.data.title} />
+      <meta property="og:image" content={Config.getImage(props.data.thumbnail)} />
+      <meta property="twitter:image" content={Config.getImage(props.data.thumbnail)} />
       <meta property="og:image:width" content="2048" />
       <meta property="og:image:height" content="1268" />
       <meta property="twitter:image:width" content="2048" />
