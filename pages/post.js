2bc0c2323bd3f13d8c406340bd0ddbc8409a39aa
Loc Du <locdu@kolabs.co>
2019-03-06 16:57:47 +0700

    update code

--- pages/post.js
+++ pages/post.js
@@ -62,12 +62,10 @@ const Post = (props) => (
         {props.data.title}
       </title>
       <meta property="og:title" content={props.data.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`http://apksafety.com/${props.data.type}/${props.data.title_slug}`} />
       <meta property="og:image" content={Config.getImage(props.data.thumbnail)} />
       <meta property="og:description" content={props.data.title} />
-      <meta property="og:url" content={`http://apksafety.com/${props.data.type}/${props.data.title_slug}`} /> 
-      {/* <meta name="description" content={props.data.title} />
-      <meta property="og:title" content={props.data.title} />
-      <meta property="og:url" content={`http://apksafety.com/${props.data.type}/${props.data.title_slug}`} /> */}
     </Head>
     <DetailFormView data={props.data} />
     <script
