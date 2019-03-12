37c77d16c80d6e239f2e73ecef92d5c46c7f80c4
Loc Du <locdu@kolabs.co>
2019-03-12 11:12:08 +0700

    u[date code

--- pages/_document.js
+++ pages/_document.js
@@ -14,8 +14,7 @@ export default class SiteDocument extends Document {
       <html>
         <Head>
           {styleTags}
-          <link rel="shortcut icon" href={`${Config.API_LOCAL_IMAGE}img/fav.ico`} />
-
+          <link rel="shortcut icon" href={`${Config.API_LOCAL_IMAGE}img/fav.ico`}/>
         </Head>
         <body>
           <div className="root">
@@ -23,14 +22,18 @@ export default class SiteDocument extends Document {
           </div>
           <NextScript />
           <script src="http://apksafety.com/static/js/vendor/bootstrap.min.js"></script>
-          <script src='http://apksafety.com/static/js/jquery.matchHeight-min.js'></script>
-          <script src="http://apksafety.com/static/js/index.js"></script>
+          <script src="http://apksafety.com/static/js/popper.min.js"></script>
           <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
           <script src="http://apksafety.com/static/js/superfish.min.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.ajaxchimp.min.js"></script>
           <script src="http://apksafety.com/static/js/jquery.magnific-popup.min.js"></script>
           <script src="http://apksafety.com/static/js/owl.carousel.min.js" defer async>></script>
           <script src="http://apksafety.com/static/js/jquery.tabs.min.js"></script>
-          <script src="http://apksafety.com/static/js/main.js"></script>
+          <script src="http://apksafety.com/static/js/mn-accordion.js"></script>
+          <script src="http://apksafety.com/static/js/jquery.nice-select.min.js"></script>
+          <script src="http://apksafety.com/static/js/mail-script.js"></script>
+          <script id="slideIndex">
+          </script>
         </body>
       </html>
     )
