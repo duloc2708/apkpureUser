087136d0ccd89f61d25b266b07840e10cead319e
Loc Du <locdu@kolabs.co>
2019-03-12 11:46:42 +0700

    u[date code

--- src/modules/home.content/ContentFormView.js
+++ src/modules/home.content/ContentFormView.js
@@ -1,20 +1,31 @@
-import PopularFormView from 'modules/home.content/PopularFormView.js'
+import SlideFormView from 'modules/home.content/SlideFormView.js'
 import GameHotFormView from 'modules/home.content/GameHotFormView.js'
 import VideoFormView from 'modules/home.content/VideoFormView.js'
 import ListTypeGame from 'modules/home.content/ListTypeGame.js'
 import TopDownFormView from 'modules/home.content/TopDownFormView.js'
 import FeaturedGame from 'modules/home.content/FeaturedGame.js'
 import FeaturedApp from 'modules/home.content/FeaturedApp.js'
-
+import SlideMobileFormView from 'modules/home.content/SlideMobileFormView.js'
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
 class ContentFormView extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <PopularFormView />
+                <BrowserView>
+                   <SlideFormView />
+                </BrowserView>
+                <MobileView>
+                   <SlideMobileFormView />
+                </MobileView>
                 <GameHotFormView />
-                <TopDownFormView/>
-                <FeaturedGame/>
-                <FeaturedApp/>
+                <TopDownFormView />
+                <FeaturedGame />
+                <FeaturedApp />
                 <VideoFormView />
                 <ListTypeGame />
             </React.Fragment>
