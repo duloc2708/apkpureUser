cdd3d61eb688bc0b1df9582d67fe12a5accd6d07
Loc Du <locdu@kolabs.co>
2019-03-21 21:08:39 +0700

    u

--- static/js/vendor/slider_blog_detail.js
+++ static/js/vendor/slider_blog_detail.js
@@ -1,51 +1,54 @@
 if ($(".gallery-item")[0] && $('.gallery-carousel')[0]) {
 	console.log('gallery', $('.gallery-carousel')[0]);
+	if ($.isFunction('owlCarousel')) {
+		$('.gallery-carousel').owlCarousel({
+			nav: true,
+			navText: [],
+			margin: 10,
+			loop: true,
+			autoplay: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				480: {
+					items: 2
+				},
+				768: {
+					items: 3
+				},
+				992: {
+					items: 2
+				}
+			}
+		});
+		$('.popup-gallery').magnificPopup({
+			delegate: '.owl-item:not(.cloned) a',
+			type: 'image',
+			removalDelay: 500, //delay removal by X to allow out-animation
+			callbacks: {
+				beforeOpen: function () {
+					// just a hack that adds mfp-anim class to markup 
+					this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+					this.st.mainClass = this.st.el.attr('data-effect');
+				}
+			},
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function (item) {
+					return item.el.attr('title') + '<small></small>';
+				}
+			}
+		});
+	}
 
-	$('.gallery-carousel').owlCarousel({
-		nav: true,
-		navText: [],
-		margin: 10,
-		loop: true,
-		autoplay: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			480: {
-				items: 2
-			},
-			768: {
-				items: 3
-			},
-			992: {
-				items: 2
-			}
-		}
-	});
-	$('.popup-gallery').magnificPopup({
-		delegate: '.owl-item:not(.cloned) a',
-		type: 'image',
-		removalDelay: 500, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup 
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function (item) {
-				return item.el.attr('title') + '<small></small>';
-			}
-		}
-	});
+
 }
 
