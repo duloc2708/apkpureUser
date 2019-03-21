3d405a82363b65d65ab9de8f9ed9cf5f1c8995e3
Loc Du <locdu@kolabs.co>
2019-03-21 22:17:51 +0700

    u

--- pages/_document.js
+++ pages/_document.js
@@ -16,18 +16,15 @@ export default class SiteDocument extends Document {
           {styleTags}
           <link rel="shortcut icon" href={`${Config.API_LOCAL_IMAGE}img/fav.ico`} />
           <meta name="referrer" content="origin" />
+          <script src="https://apksafety.com/static/js/vendor/jquery-2.2.4.min.js"></script>
+
         </Head>
         <body>
           <div className="root">
             {main}
           </div>
           <NextScript />
-          {/* <script async="" id="__NEXT_PAGE__/" src="/_next/-/page/index.js">
-          </script>
-          <script async="" id="__NEXT_PAGE__/_app" src="/_next/-/page/_app.js"></script>
-          <script async="" id="__NEXT_PAGE__/_error" src="/_next/-/page/_error.js"></script>
-          <script src="/_next/static/commons/manifest.js"></script>
-          <script src="/_next/static/commons/main.js" /> */}
+
           <script src="https://apksafety.com/static/js/vendor/bootstrap.min.js"></script>
           <script src="https://apksafety.com/static/js/superfish.min.js"></script>
           <script src="https://apksafety.com/static/js/jquery.magnific-popup.min.js"></script>
