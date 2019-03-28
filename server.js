d65d7bafed1ea35a100550fee49140cefeca7ba7
Loc Du <locdu@kolabs.co>
2019-03-28 11:12:32 +0700

    u

--- server.js
+++ server.js
@@ -22,17 +22,18 @@ app.prepare()
         res.setHeader("Cache-Control", "public,max-age=31536000,immutable");
       }
     }));
+    server.use('/fonts', express.static(__dirname + '/fonts/', {
+      setHeaders(res) {
+        res.setHeader("Cache-Control", "public,max-age=31536000,immutable");
+      }
+    }))
     server.use(function (req, res, next) {
       res.setHeader('Cache-Control', 'public, max-age=31536000, no Etag, no Last-Modified'); // 1 year
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, cache-control");
       next();
     });
-    server.use('/fonts', express.static(__dirname + '/fonts/', {
-      setHeaders(res) {
-        res.setHeader("Cache-Control", "public,max-age=31536000,immutable");
-      }
-    }))
+
     // parse application/x-www-form-urlencoded
     server.use(bodyParser.urlencoded({ extended: false }))
 
