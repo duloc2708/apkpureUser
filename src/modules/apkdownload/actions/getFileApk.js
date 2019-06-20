a7c13145620994ce0142545a46fff296bf54aa7c
Loc Du <locdu@kolabs.co>
2019-06-20 16:30:39 +0700

    upd

--- src/modules/apkdownload/actions/getFileApk.js
+++ src/modules/apkdownload/actions/getFileApk.js
@@ -1,13 +1,13 @@
 
 export default (idgame = '') => {
     return new Promise((resolve, rejects) => {
-        axios.post(`http://61.28.230.226:3001/api/test/getlink`, { id: idgame })
+        axios.post(`${Config.API_URL}getapk`, { id: idgame })
             .then(function (response) {
-                let { data } = response.data
+                let { Data } = response.data
                 axios({
                     method: 'post',
                     url: `https://api-apk.evozi.com/download`,
-                    data: data,
+                    data: Data,
                     config: { headers: { 'Content-Type': 'multipart/form-data' } }
                 })
                     .then(function (response2) {
